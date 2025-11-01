eb1c25a857421a35d56e5b63dfae0de4e10a0ec3
vicky040 <vickiitd3125@gmail.com>
2025-11-01 20:58:53 +0530

    resolve server issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['silent-melisent-cryptoo-bba700a0.koyeb.app'], // Add the allowed host here
+  },
 })
